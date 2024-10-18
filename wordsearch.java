592c3f0e85e072261fc1c6674ee2d825db9b1e06
R.M.McKernan <WhatLibrarian@users.noreply.github.com>
2024-10-17 19:10:17 -0700

    Created 2 methods to pull code out of main.  Now broken verticals.

--- wordsearch.java
+++ wordsearch.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //// https://gist.github.com/R3DHULK/b21adcd7bd8904507023880f80934ca0
 
 public class wordsearch {
-    // objects
+    // constants object file
     public static final int maxWordCount = 5; // max number of words
     public static final int maxWordLength = 8; // max word length
     public static final int gridSize = (maxWordLength * 3); // in case bigger grid needed
@@ -20,6 +20,7 @@ public class wordsearch {
         Scanner wordSc = new Scanner(System.in);
         String[] words = new String[maxWordCount]; // single array to store user words
         int wordCount = 0; // tracks number of words entered. Starts at 0
+        
         // While loop to accept user words and validate (for bonus points!)
         while (wordCount < maxWordCount) {
             System.out.println("Enter word " + (wordCount + 1) + ": ");
@@ -31,27 +32,24 @@ public class wordsearch {
             } else {
                 System.out.println("Invalid word. Make sure it's alphabetic, under " + maxWordLength
                         + " characters and with no spaces.");
-            }} 
-            
+                }
+            }   
         // end Method to ask for, accept and validate users words.
         
-
-        // create the grid
-        char[][] grid = new char[gridSize][gridSize];
-
-        // Fill grid with default placeholder - replace with chars later! Helps with
+        // create the grid and fill with non-alpha chars for 
         // visual representation in troubleshooting
+        char[][] grid = new char[gridSize][gridSize];
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
                 grid[i][j] = '-';
                 }
-            }
+        }
 
         // randomly place words in grid verticial and horizontal (not diagonal)
         Random randPlacement = new Random(); // will pick a random int to use to place words
 
         // begin filling the grid with the users words.
-        for (String placeWord : words) {
+        for (String placeWord : words) {  // start of for loop to place words in grid
             boolean placed = false; // validate word is not already placed
 
             while (!placed) { // ! = NOT operator https://www.shehackspurple.dev/what-does-mean-in-java-7
@@ -60,58 +58,45 @@ public class wordsearch {
 
                 if (isHorz) {
                     startRow = randPlacement.nextInt(gridSize);
-                    startCol = randPlacement.nextInt(gridSize - placeWord.length()); // isn't outside the grid
-                   
-                //method to check to see if word can be placed in-bounds
-                boolean canPlace = true;
-                for (int i = 0; i < placeWord.length(); i++) {
-                      if (grid[startRow][startCol + i] != '-' && grid[startRow][startCol + i] != placeWord.charAt(i)) {
-                                canPlace = false;
-                                break;
-                        } // end of if loop
-                } // end of for check placement method. 
-
-                // places the words horizontally
-                if (canPlace) {
-                    for (int i = 0; i < placeWord.length(); i++) {
-                        grid[startRow][startCol + i] = placeWord.charAt(i);
-                        }
-                        placed = true;  // mark the word as being placed
+                    startCol = randPlacement.nextInt(gridSize - placeWord.length()); // isn't outside the grid horizontally
+                } else {
+                    startRow = randPlacement.nextInt(gridSize - placeWord.length()); //isn't outside the grid vertically
+                    startCol = randPlacement.nextInt(gridSize); 
                     }
-            } else { // vertical placement
-                  startRow = randPlacement.nextInt(gridSize - placeWord.length()); // isn't outside the grid
-                  startCol = randPlacement.nextInt(gridSize);   
+                  
+                // Can the word be placed?
+                if (canPlaceWord (grid, placeWord, startRow, startCol, isHorz)) { // if the method canPlaceWord
+                    placeWordInGrid (grid, placeWord, startRow, startCol, isHorz);
+                    placed = true; // Now that word is marked as placed.
+                    } // end while loop
+            } // end of while loop
+        } // end of for loop
 
-                // Check if word can be placed
-                boolean canPlace = true;
-                for (int i = 0; i < placeWord.length(); i++) {
-                    if (grid[startRow + i][startCol] != '-' && grid[startRow + i][startCol] != placeWord.charAt(i)) {
-                        canPlace = false;
-                        break;
-                        }
-                    }
-            
-        // Place the word vertically
-                    if (canPlace) {
-                        for (int i = 0; i < placeWord.length(); i++) {
-                            grid[startRow + i][startCol] = placeWord.charAt(i);
-                        }
-                        placed = true;         
-                    } //end of can place if loop
-                } //end of else loop
-            } // end of while 
-         } // end of for loop
+            // Print the final grid
+            printGrid(grid);
+} // end of Main
 
-        // Print the final grid
-        printGrid(grid);
-
+// Method to check if the word can be placed in the grid
+private static boolean canPlaceWord(char[][] grid, String word, int startRow, int startCol, boolean isHorz) {
+    for (int i = 0; i < word.length(); i++) {
+        char gridChar = isHorz ? grid[startRow][startCol + i] : grid[startRow + i][startCol];
+        if (gridChar != '-' && gridChar != word.charAt(i)) {
+            return false; // Cannot place the word
+        }
     }
+    return true; // Can place the word
+}
 
-// Method to check if the word fits and can overlap
-    //public static void bePlace(){
-      
-  //  }
-// end of if can Place Method
+// Method to place the word in the grid
+public static void placeWordInGrid(char[][] grid, String word, int startRow, int startCol, boolean isHorz) {
+    for (int i = 0; i < word.length(); i++) {
+        if (isHorz) {
+            grid[startRow][startCol + i] = word.charAt(i);
+        } else {
+            grid[startRow + i][startCol] = word.charAt(i);
+        }
+    }
+}
 
 // Method to print Grid with words
 public static void printGrid(char[][] grid) {
@@ -131,4 +116,3 @@ public static void intro() {// Introductory Text
 } // end of intro text
 
 } // end of class
-//updated
